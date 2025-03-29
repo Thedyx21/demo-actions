ca3bc76018f0477cf54d07a3c6a587d313252cbe
thedyx21 <dylan.drouet@uees.edu.ec>
2025-03-29 11:53:14 -0500

    ojala funcione

---
+++ src/main.py
@@ -0,0 +1,12 @@
+#src/main.py 
+
+import os
+
+# obtener entrada
+valor= os.version.get("Input_Value")
+
+# mostrar resultado en consola
+if "GITHUB_OUTPUT" in os.environ: 
+    with open(os.environ["GITHUB_OUTPUT"], "a") as f:
+        print("{0}=Hola {1}".format('result', valor), file=f) 
+print("hola")

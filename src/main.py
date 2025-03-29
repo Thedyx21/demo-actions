a2aede921e193e751ea9a7c9d31c363b2b0751b8
thedyx21 <dylan.drouet@uees.edu.ec>
2025-03-29 11:56:28 -0500

    si va a funcionar

--- src/main.py
+++ src/main.py
@@ -10,3 +10,4 @@ if "GITHUB_OUTPUT" in os.environ:
     with open(os.environ["GITHUB_OUTPUT"], "a") as f:
         print("{0}=Hola {1}".format('result', valor), file=f) 
 print("hola")
+print("chao")
